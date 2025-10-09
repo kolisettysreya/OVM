00e6bfa2fd4833bc17e6a70dc7919299537bc01f
KOLISETTY SREYA SRI <kolisettysreya25@gmail.com>
2025-10-09 18:56:17 +0530

    Update OvmApplication.java

--- ovm/src/main/java/ovm/OvmApplication.java
+++ ovm/src/main/java/ovm/OvmApplication.java
@@ -2,17 +2,20 @@ package ovm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class OvmApplication {
+public class OvmApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OvmApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(OvmApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(OvmApplication.class, args);
+    }
 }
